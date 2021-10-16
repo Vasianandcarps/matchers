7b2f3d7f241b14bdf792ca8000eb120fc8e305c1
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-10-16 11:50:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let n = 13;
+let str = " ";
+function matchers() {
+  matcher();
+  document.write(n);
+
+  alert(n + str);
+  while (n > 0) {
+    alert("Your turn!");
+    let m = prompt("Get the matches!");
+    if (isNaN(m) || m < 0 || m > 3) {
+      break;
+    }
+    n -= m;
+    matcher();
+    alert(n + str);
+    alert("my turn!");
+    let rand = Math.floor(Math.random() * 3) + 1;
+    alert(rand);
+    n -= rand;
+    matcher();
+    if (n <= 0) {
+      alert("You win!");
+      break;
+    } else if (n <= rand) {
+      alert("I win!");
+      break;
+    }
+  }
+}
+function matcher() {
+  for (let i = 0; i < n; i++) {
+    str += "| ";
+  }
+}
